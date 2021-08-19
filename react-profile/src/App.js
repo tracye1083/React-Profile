c9b003044732df1950180894d1a07ddf80fd1e7f
Tracy Angell <63623364+tracye1083@users.noreply.github.com>
2021-08-19 18:09:26 -0400

    Update App.js

--- react-profile/src/App.js
+++ react-profile/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import React from 'react';
+
+import Header from './components/Header';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import Navigation from './components/Nav';
+import About from './components/About';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navigation />
+      <Header />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+      </div>
   );
 }
 
